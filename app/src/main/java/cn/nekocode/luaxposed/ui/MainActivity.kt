29548bb742c220ab1d0dcb0f97cac96bf59904c5
nekocode <nekocode.cn@gmail.com>
2018-08-02 23:10:24 +0800

    Initial commit

---
+++ app/src/main/java/cn/nekocode/luaxposed/ui/MainActivity.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2017 nekocode (nekocode.cn@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package cn.nekocode.luaxposed.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import cn.nekocode.luaxposed.R
+
+/**
+ * @author nekocode (nekocode.cn@gmail.com)
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
